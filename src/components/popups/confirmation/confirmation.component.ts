72469f6f3f90c49d2684f78011bd3369f4f50fcf
Alejandro <alejandro.moncal99@gmail.com>
2025-12-05 15:18:20 +0100

    Add components and styles

---
+++ src/components/popups/confirmation/confirmation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirmation',
+  imports: [],
+  templateUrl: './confirmation.component.html',
+  styleUrl: './confirmation.component.css'
+})
+export class ConfirmationComponent {
+
+}
---
+++ src/components/prediction/prediction.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prediction',
+  imports: [],
+  templateUrl: './prediction.component.html',
+  styleUrl: './prediction.component.css'
+})
+export class PredictionComponent {
+
+}
